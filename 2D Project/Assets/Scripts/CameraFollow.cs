d9fe0706d195f0b22eff625d5c5a8a4631433b8b
Spraxs <Spraxs@outlook.com>
2017-11-03 16:41:03 +0100

    animations movement

---
+++ 2D Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    private Vector2 velocity;
+
+
+    public bool cameraSlide = false;
+
+    public float smoothTimeYFollow;
+    public float smoothTimeXFollow;
+
+    public float smoothTimeYSlide;
+    public float smoothTimeXSlide;
+    public float maxDistanceXSlide = 5f;
+    public float maxDistanceYSlide = 3f;
+    public float distanceSlide = 0.1f;
+
+
+
+    public GameObject player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+
+
+        if (cameraSlide)
+        {
+            float minPosX = Mathf.SmoothDamp(transform.position.x, transform.position.x - distanceSlide, ref velocity.x, smoothTimeXSlide);
+            float plusPosX = Mathf.SmoothDamp(transform.position.x, transform.position.x + distanceSlide, ref velocity.x, smoothTimeXSlide);
+
+            float minPosY = Mathf.SmoothDamp(transform.position.y, transform.position.y - distanceSlide, ref velocity.y, smoothTimeXSlide);
+            float plusPosY = Mathf.SmoothDamp(transform.position.y, transform.position.y + distanceSlide, ref velocity.y, smoothTimeXSlide);
+
+            if (player.transform.position.x - transform.position.x > maxDistanceXSlide)
+            {
+                transform.position = new Vector3(plusPosX, transform.position.y, transform.position.z);
+            }
+
+            if (player.transform.position.x - transform.position.x < -maxDistanceXSlide)
+            {
+                transform.position = new Vector3(minPosX, transform.position.y, transform.position.z);
+            }
+
+            if (player.transform.position.y - transform.position.y > maxDistanceYSlide)
+            {
+                transform.position = new Vector3(transform.position.x, plusPosY, transform.position.z);
+            }
+
+            if (player.transform.position.y - transform.position.y < -maxDistanceYSlide)
+            {
+                transform.position = new Vector3(transform.position.x, minPosY, transform.position.z);
+            }
+        } else
+        {
+            float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeXFollow);
+            float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeYFollow);
+
+            transform.position = new Vector3(posX, posY, transform.position.z);
+        }
+    }
+}
---
+++ 2D Project/Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Floats
+    public float maxSpeed = 3f;
+    public float speed = 50f;
+    public float jumpPower = 200;
+
+    // Booleans
+    public bool grounded;
+    public bool canDoubleJump;
+
+    // References
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                rb2d.AddForce(Vector2.up * jumpPower);
+                canDoubleJump = true;
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    canDoubleJump = false;
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                    rb2d.AddForce(Vector2.up * jumpPower / 1.75f);
+                }
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+        // Fake friction / Easing the x speed of our player
+
+        if (grounded)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+
+        //rb2d.AddForce(((Vector2.right * (speed * 10)) * h) * Time.deltaTime);
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+}
